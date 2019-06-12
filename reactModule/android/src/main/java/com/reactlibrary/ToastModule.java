5f42684457c3b78fdf945c69206e33ad44987081
Dariusz Konieczko <dkonieczko@netizens.pl>
2019-06-12 12:12:01 +0200

    Sample toast module

---
+++ reactModule/android/src/main/java/com/reactlibrary/ToastModule.java
@@ -0,0 +1,44 @@
+package com.reactlibrary;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Example based on: https://facebook.github.io/react-native/docs/native-modules-android
+ */
+public class ToastModule extends ReactContextBaseJavaModule {
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "Toast example";
+    }
+
+    // Optional
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>(10);
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+
+
+}
