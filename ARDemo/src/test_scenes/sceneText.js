5fd9a1954e6e5e566a73a65976dff5e3e81c593c
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-07-12 10:57:23 +0200

    Update sceneText.

--- ARDemo/src/test_scenes/sceneText.js
+++ ARDemo/src/test_scenes/sceneText.js
@@ -64,11 +64,43 @@ class SceneText extends React.Component {
         });
     }
 
+    renderTexts() {
+        const texts = [
+            'The quick brown fox jumps over the lazy dog.',
+            'With time and patience the mulberry leaf becomes a silk gown.',
+            'Man is a pupil, pain is his teacher.',
+            'A journey of a thousand miles begins with a single step.',
+            'A smile will gain you ten more years of life.',
+            'He who asks is a fool for five minutes, but he who does not ask remains a fool forever.',
+        ];
+        const textAlignments = ['center', /*'justify',*/ 'left', 'right'];
+        const size = 0.15;
+        return texts.map((text, index) => {
+            const y = -size * index;
+            const textAlignment = textAlignments[(index / 2) % textAlignments.length];
+            const wrap = (index >= (textAlignments.length / 2));
+            return (
+                <text 
+                    key={index} 
+                    localPosition={[0, y, 0]} 
+                    textAlignment={textAlignment}
+                    textColor={[1,1,1,0.8]} 
+                    textSize={0.08}
+                    wrap={wrap}
+                >{text}</text>
+            );
+        });
+    }
+
     render() {
-        // const contents = 'abcdefghijklmnopqrstuvxyz';
         return (
             <view localPosition={this.props.localPosition}>
-                {this.renderLetters()}
+                <view localPosition={[0,0.3,0]}>
+                    {this.renderLetters()}
+                </view>
+                <view localPosition={[0,-1.4,0]}>
+                    {this.renderTexts()}
+                </view>
             </view>
         );
     }
