220654c4a0711c20386288922ef84c9444949e8e
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-04 14:10:28 +0200

    Set debug mode on.

--- ARDemo/proxy/magic-script/MagicScript.js
+++ ARDemo/proxy/magic-script/MagicScript.js
@@ -3,9 +3,9 @@ import { ReactNativeMagicScript } from 'react-native-magic-script';
 import ReactNativeApp from '../react-native/ReactNativeApp';
 
 const MagicScript = {
-    registerApp: (name, appComponent) => {
+    registerApp: (name, appComponent, debug = false) => {
         AppRegistry.registerComponent(name, () => ReactNativeApp);
-        ReactNativeMagicScript.render(appComponent, { name: 'root' });
+        ReactNativeMagicScript.render(appComponent, { name: 'root' }, null, debug);
     }
 };
 
