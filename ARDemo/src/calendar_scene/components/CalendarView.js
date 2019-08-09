568cbe5fa9edd4defbcd2a54fd27ec24eb6a4fd0
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-09 15:00:15 +0200

    Update to calendar scene.

--- ARDemo/src/calendar_scene/components/CalendarView.js
+++ ARDemo/src/calendar_scene/components/CalendarView.js
@@ -4,9 +4,10 @@ import React from "react";
 import { BASE_URL, CALENDAR, ROOM } from "../config/Client";
 import { PRISM_COLOR, TEXT_COLOR } from "../config/Colors";
 import { PRISM_X, PRISM_Y, UPDATE_FREQUENCY } from "../config/Sizes";
-
-
 import CalendarItem from "./CalendarItem";
+
+const initialEventsUrl = "https://firebasestorage.googleapis.com/v0/b/components-storage.appspot.com/o/events_initial.json?alt=media&token=c445d8a0-c4c4-4d7f-83b7-a42289bfd93a";
+const allEventsUrl = "https://firebasestorage.googleapis.com/v0/b/components-storage.appspot.com/o/events_all.json?alt=media&token=309a34e5-fa42-4c17-aa79-9400e0b01a2d";
 
 export default class CalendarView extends React.Component {
   constructor(props) {
@@ -20,18 +21,18 @@ export default class CalendarView extends React.Component {
   }
 
   async componentDidMount() {
-		await this.updateCalendar();
-		if (this.interval === undefined) {
-			this.interval = setInterval(async () => await this.updateCalendar(), UPDATE_FREQUENCY);
-		}
+		await this.updateCalendar(initialEventsUrl);
+		// if (this.interval === undefined) {
+		// 	this.interval = setInterval(async () => await this.updateCalendar(), UPDATE_FREQUENCY);
+		// }
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    // clearInterval(this.interval);
   }
 
-  async updateCalendar() {
-    const data = await this.fetchJsonData(`${BASE_URL}`/*/${CALENDAR}`*/).catch(
+  async updateCalendar(url) {
+    const data = await this.fetchJsonData(url).catch(
       e => {
         this.setState(state => ({
           message: e.message
@@ -74,29 +75,63 @@ export default class CalendarView extends React.Component {
     return <view name="events-list">{calendarView}</view>;
   }
 
+  onInitialEventsClick = () => {
+    this.updateCalendar(initialEventsUrl);
+  }
+
+  onMoreEventsClick = () => {
+    this.updateCalendar(allEventsUrl);
+  }
+
+  renderControlPanel() {
+    return (
+      <view localPosition={[0, 0.5, 0]}>
+        <button
+          localPosition={[-0.25, 0, 0]}
+          width={0.25}
+          height={0.10}
+          roundness={0.5}
+          textSize={0.035}
+          onClick={this.onInitialEventsClick}
+        >Initial events</button>
+        <button
+          localPosition={[0.25, 0, 0]}
+          width={0.25}
+          height={0.10}
+          roundness={0.5}
+          textSize={0.035}
+          onClick={this.onMoreEventsClick}
+        >More events</button>
+      </view>
+    );
+  }
+
   render() {
     const calendarView = this.renderCalendarView(this.state.space);
     return (
-      <view name="main-view">
-        <text
-          textSize={0.05}
-          textColor={TEXT_COLOR}
-          width={PRISM_X}
-          alignment={"top-center"}
-          localPosition={[0, 0.32, 0]}
-        >
-          {"Schedule for room"}
-        </text>
-        <text
-          textSize={0.05}
-          textColor={TEXT_COLOR}
-          width={0.64}
-          alignment={"top-center"}
-          localPosition={[0, 0.27, 0]}
-        >
-          {ROOM}
-        </text>
-        {calendarView}
+      <view name="main-view" localPosition={this.props.localPosition}>
+        {this.renderControlPanel()}
+        <view>
+          <text
+            textSize={0.05}
+            textColor={TEXT_COLOR}
+            width={PRISM_X}
+            alignment={"top-center"}
+            localPosition={[0, 0.32, 0]}
+          >
+            {"Schedule for room"}
+          </text>
+          <text
+            textSize={0.05}
+            textColor={TEXT_COLOR}
+            width={0.64}
+            alignment={"top-center"}
+            localPosition={[0, 0.27, 0]}
+          >
+            {ROOM}
+          </text>
+          {calendarView}
+        </view>
       </view>
     );
   }
--- ARDemo/src/calendar_scene/config/Client.js
+++ ARDemo/src/calendar_scene/config/Client.js
@@ -1,4 +1,4 @@
 //
-export const BASE_URL = "https://firebasestorage.googleapis.com/v0/b/components-storage.appspot.com/o/events.json?alt=media&token=78d8717b-8510-4ca0-bad2-d3b0a4558764";
+export const BASE_URL = "";
 export const CALENDAR = "";
 export const ROOM = "Mira";
