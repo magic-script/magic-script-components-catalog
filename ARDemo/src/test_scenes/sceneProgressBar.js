50651238513e95d9fedca7a2cf1a6d715b42c370
Sebastian Paluchiewicz <sebastian.paluchiewicz@sofomo.com>
2019-08-12 14:26:50 +0200

    Added animation for progress update.

--- ARDemo/src/test_scenes/sceneProgressBar.js
+++ ARDemo/src/test_scenes/sceneProgressBar.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 class SceneProgressBar extends React.Component {
+  state = { progressValue: 0 }
+
+  componentDidMount() {
+    this.updateProgress();
+    this.handler = setInterval(this.updateProgress, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.handler);
+  }
+
+  updateProgress = () => {
+    var { progressValue } = this.state;
+    if (progressValue <= 100) {
+      progressValue += 10;
+    } else {
+      progressValue = 0;
+    }
+    this.setState({ progressValue });
+  }
+
   render () {
+    const { progressValue } = this.state;
     return (
       <view localPosition={this.props.localPosition}>
         <progressBar 
@@ -12,7 +34,7 @@ class SceneProgressBar extends React.Component {
         <progressBar 
           alignment={'center-center'}
           localPosition={[0, 0.0, 0]} 
-          value={0.66}
+          value={ progressValue / 100 }
           progressColor={{
             beginColor: [0.1, 0.5, 0.9, 1],
             endColor: [0.1, 0.9, 0.5, 1]
