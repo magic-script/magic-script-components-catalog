b4b23eb1fad5b383dcc57157777ec49231a620db
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-12 09:43:17 +0200

    Update progressColor format in sceneProgressBar.

--- ARDemo/src/test_scenes/sceneProgressBar.js
+++ ARDemo/src/test_scenes/sceneProgressBar.js
@@ -13,8 +13,10 @@ class SceneProgressBar extends React.Component {
           alignment={'center-center'}
           localPosition={[0, 0.0, 0]} 
           value={0.66}
-          startColor={[0.83, 0.8, 0.2, 1]} 
-          endColor={[0.2, 0.5, 0.7, 1]}
+          progressColor={{
+            beginColor: [0.83, 0.8, 0.2, 1],
+            endColor: [0.2, 0.5, 0.7, 1]
+          }}
           width={0.75}
           height={0.05}
         />
@@ -22,7 +24,9 @@ class SceneProgressBar extends React.Component {
           alignment={'center-center'}
           localPosition={[0, -0.4, 0]} 
           value={0.9} min={-10} max={10}
-          startColor={[1, 0.2, 0.2, 1]}  
+          progressColor={{
+            beginColor: [1, 0.2, 0.2, 1]
+          }}
           width={1}
           height={0.1}
         />
