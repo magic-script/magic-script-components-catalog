46fb14acf839b9922c7e9fb91e63ef56d1cb541f
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-21 11:51:16 +0200

    Update path in script reload-mxs-module.

--- reload-mxs-module.sh
+++ reload-mxs-module.sh
@@ -2,6 +2,6 @@ printf "\033c"
 yarn remove react-native-magic-script
 # rm -rf ./node_modules/react-native-magic-script
 echo "react-native-magic-script package removed"
-yarn add ../../react-native-magic-script
+yarn add ../react-native-magic-script
 echo "react-native-magic-script package added"
 say -v Melina done
