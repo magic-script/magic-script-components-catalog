05949b0126f98eb451c8fc5294e1e1d17f99dfde
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-21 10:25:05 +0200

    #56 Rename iOS project (ARDemo -> Catalog).

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { MagicScript } from './proxy';
 import BrowserApp from './src/browser_app';
 
-MagicScript.registerApp('ARDemo', <BrowserApp />, false);
+MagicScript.registerApp('Catalog', <BrowserApp />, false);
