abf702016b1496dec2dbb3551ba9022cacb2d796
Dariusz Konieczko <dkonieczko@netizens.pl>
2019-08-30 09:25:23 +0200

    tag fix

--- src_mobile/test_scenes/sceneSpinner.js
+++ src_mobile/test_scenes/sceneSpinner.js
@@ -5,20 +5,20 @@ class SceneSpinner extends React.Component {
   render() {
     return (
       <view localPosition={this.props.localPosition}>
-        <Spinner 
+        <spinner 
           alignment={'top-center'}
           localPosition={[-0.2, 0, 0]} 
           height={0.2}
         />
         
-        <Spinner 
+        <spinner 
           alignment={'bottom-center'}
           localPosition={[0.2, 0, 0]} 
           height={0.4}
           localScale={[0.5,0.5,0]}
         />
 
-         <Spinner 
+        <spinner 
             alignment={'center-center'}
             localPosition={[0, -0.5, 0]} 
         />
