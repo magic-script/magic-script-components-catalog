3459dd8b40124ae9a518757584c7c41df658a422
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-09-09 13:31:05 +0200

    Optimize lines rendering.

--- src_app/test_scenes/sceneLine.js
+++ src_app/test_scenes/sceneLine.js
@@ -13,14 +13,14 @@ class SceneLine extends React.Component {
 	}
 
 	increaseVertexCount() {
-		const count = this.state.points.length / 2;
+		const count = this.state.points.length - 1;
 		if (count < 20) {
 			this.updatePoints(count + 1);
 		}
 	}
 
 	decreaseVertexCount() {
-		const count = this.state.points.length / 2;
+		const count = this.state.points.length - 1;
 		if (count > 3) {
 			this.updatePoints(count - 1);
 		}
@@ -29,16 +29,15 @@ class SceneLine extends React.Component {
 	updatePoints(count) {
 		const radius = 0.4;
 		const points = [];
-		const p0 = [0, radius, 0];
-		points.push(p0);
-		for (var i = 1; i < count; ++i) {
+		for (var i = 0; i < count; ++i) {
 			const angle = (2 * Math.PI) * (i / count);
 			const x = radius * Math.sin(angle);
 			const y = radius * Math.cos(angle);
 			points.push([x, y, 0]);
-			points.push([x, y, 0]);
-		}
-		points.push(p0);
+    }
+    if (points.length > 0) {
+      points.push(points[0]);
+    }
 		this.setState({ points });
 	}
 	
@@ -52,7 +51,7 @@ class SceneLine extends React.Component {
           alignment={'center-center'}
           localPosition={[0, -0.15, 0]} 
           textSize={0.1} 
-        >{`${points.length / 2} segments`}</text>
+        >{`${points.length - 1} segments`}</text>
         <button localPosition={[-0.3, -0.35, 0]} width={0.3} height={0.15} textSize={0.1} onClick={this.decreaseVertexCount}>-</button>
       </view>
     );
