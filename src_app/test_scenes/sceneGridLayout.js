6695d059b2731c107379e67628cbf0a334ee03aa
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-09-13 21:22:24 +0200

    Add defaultItemPadding to gridLayout scene.

--- src_app/test_scenes/sceneGridLayout.js
+++ src_app/test_scenes/sceneGridLayout.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class SceneGridLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    // The padding order is:  top, right, bottom, left
+    this.state = { padding: [0,0,0,0] };
+  }
+
   renderItems (columns, rows) {
     var items = [];
     const itemsCount = rows * columns;
@@ -16,21 +22,54 @@ class SceneGridLayout extends React.Component {
       }
     }
 
-    return items.map((item, index) => <text key={index} textSize={(index === indexMidRow) ? 0.2 : 0.08}>{item}</text>);
+    return items.map((item, index) => <text debug={true} key={index} textSize={(index === indexMidRow) ? 0.2 : 0.08}>{item}</text>);
+  }
+
+  onTopToggleChanged = () => {
+    this.togglePaddingAt(0);
+  }
+
+  onRightToggleChanged = () => {
+    this.togglePaddingAt(1);
+  }
+
+  onBottomToggleChanged = () => {
+    this.togglePaddingAt(2);
+  }
+
+  onLeftToggleChanged = () => {
+    this.togglePaddingAt(3);
+  }
+
+  togglePaddingAt(index) {
+    var values = [...this.state.padding];
+    values[index] = Math.abs(values[index] - 0.1);
+    this.setState({ padding: values });
   }
 
   render () {
+    const { padding } = this.state;
     const columns = 3;
     return (
       <view localPosition={this.props.localPosition}>
         <gridLayout
-          alignment={'center-center'}
+          debug={true}
+          alignment={'top-center'}
           columns={columns}
-          defaultItemAlignment={'center-center'}
-          defaultItemPadding={[0.03, 0.03, 0.03, 0.03]}
+          defaultItemAlignment={'top-left'}
+          defaultItemPadding={padding}
+          localPosition={[0, -0.5, 0]}
         >
           {this.renderItems(columns, 6)}
         </gridLayout>
+        <view>
+          <text localPosition={[0, 0, 0]} textSize={0.08} alignment={'bottom-center'}>Set items padding:</text>
+          <toggle localPosition={[0.4, -0.1, 0]} textSize={0.08} height={0.1} on={padding[0] > 0} onToggleChanged={this.onTopToggleChanged}>top</toggle>
+          <toggle localPosition={[0.4, -0.25, 0]} textSize={0.08} height={0.1} on={padding[1] > 0} onToggleChanged={this.onRightToggleChanged}>right</toggle>
+          <toggle localPosition={[0.4, -0.4, 0]} textSize={0.08} height={0.1} on={padding[2] > 0} onToggleChanged={this.onBottomToggleChanged}>bottom</toggle>
+          <toggle localPosition={[-0.3, -0.25, 0]} textSize={0.08} height={0.1} on={padding[3] > 0} onToggleChanged={this.onLeftToggleChanged}>left</toggle>
+        </view>
+        
       </view>
     );
   }
