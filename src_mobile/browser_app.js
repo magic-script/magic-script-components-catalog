66ee27ccd0854b10edb54370fae4a3589642eb55
unknown <mateusz.wojcik@sofomo.com>
2019-09-15 15:25:34 +0200

    added linear layout scene

--- src_mobile/browser_app.js
+++ src_mobile/browser_app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import { Button, Text, View } from 'react-native-magic-script';
 // import { SceneA, SceneB } from './demo_scenes';
-import { SceneButton, SceneGridLayout, SceneImage, SceneImageRemote, SceneLine, SceneModel, 
-  SceneTextAlignment, SceneAlignmentUpdate, SceneTextCharacters, SceneTextLetters, 
-  SceneTextMultiline, SceneTextEdit,SceneToggle, GameTicTacToe, SceneProgressBar,
-  SceneAlignment, SceneRemote, SceneSpinner, SceneVideo
+import {
+  SceneButton, SceneGridLayout, SceneImage, SceneImageRemote, SceneLine, SceneListView, SceneModel,
+  SceneTextAlignment, SceneAlignmentUpdate, SceneTextCharacters, SceneTextLetters,
+  SceneTextMultiline, SceneTextEdit, SceneToggle, GameTicTacToe, SceneProgressBar,
+  SceneAlignment, SceneRemote, SceneSpinner, SceneVideo, SceneLinearLayout
 } from './test_scenes';
 import CalendarView from './calendar_scene/components/CalendarView';
 
@@ -13,30 +14,35 @@ class BrowserApp extends React.Component {
     super(props);
 
     this.scenes = [
-      { name: 'Letters', component: <SceneTextLetters localPosition={[0, 0.75, 0]}/> },
-      { name: 'Characters', component: <SceneTextCharacters localPosition={[0, 0.75, 0]}/> },
-      { name: 'Text alignment', component: <SceneTextAlignment localPosition={[0, 0.5, 0]}/> },
-      { name: 'Text alignment (multiline)', component: <SceneTextMultiline localPosition={[0, 0.5, 0]}/> },
-      // { name: 'Alignment update', component: <SceneAlignmentUpdate localPosition={[0, 0.5, 0]}/> },
-      { name: 'Text edit', component: <SceneTextEdit localPosition={[0, 0.5, 0]}/> },
+      { name: 'Grid layout', component: <SceneGridLayout localPosition={[0, 0.5, 0]} /> },
+      { name: 'Linear layout', component: <SceneLinearLayout localPosition={[0, 0.5, 0]} /> },
+      { name: 'Alignment update', component: <SceneAlignmentUpdate localPosition={[0, 0.5, 0]}/> },
+      
+      { name: 'ListViews', component: <SceneListView /> },
+      { name: 'Letters', component: <SceneTextLetters localPosition={[0, 0.75, 0]} /> },
+      { name: 'Characters', component: <SceneTextCharacters localPosition={[0, 0.75, 0]} /> },
+      { name: 'Text alignment', component: <SceneTextAlignment localPosition={[0, 0.5, 0]} /> },
+      { name: 'Text alignment (multiline)', component: <SceneTextMultiline localPosition={[0, 0.5, 0]} /> },
+      
+      { name: 'Text edit', component: <SceneTextEdit localPosition={[0, 0.5, 0]} /> },
       // { name: 'Buttons', component: <SceneButton /> },
-      { name: 'Local images', component: <SceneImage localPosition={[0, 0.5, 0]}/> },
-      { name: 'Remote images', component: <SceneImageRemote localPosition={[0, 0, 0]}/> },
+      { name: 'Local images', component: <SceneImage localPosition={[0, 0.5, 0]} /> },
+      { name: 'Remote images', component: <SceneImageRemote localPosition={[0, 0, 0]} /> },
       // { name: 'Video', component: <SceneVideo localPosition={[0, 0, 0]}/> },
       // { name: 'Model 3D', component: <SceneModel localPosition={[0, 0, 0]}/> },
-      { name: 'Grid layout', component: <SceneGridLayout localPosition={[0, 0.5, 0]}/> },
-      { name: 'Toggle', component: <SceneToggle localPosition={[0, 0, 0]}/> },
-      { name: 'Spinner', component: <SceneSpinner localPosition={[0, 0, 0]}/> },
+      
+      { name: 'Toggle', component: <SceneToggle localPosition={[0, 0, 0]} /> },
+      { name: 'Spinner', component: <SceneSpinner localPosition={[0, 0, 0]} /> },
       // { name: 'Progress bar', component: <SceneProgressBar localPosition={[0, 0, 0]}/> },
       // // { name: '', component: <SceneA /> },
       // // { name: '', component: <SceneB /> },
       { name: 'Tic Tac Toe', component: <GameTicTacToe /> },
-      { name: 'Clock (alignment)', component: <SceneAlignment localPosition={[0, 0, 0]}/> },
-      { name: 'Line', component: <SceneLine localPosition={[0, 0, 0]}/> },
+      { name: 'Clock (alignment)', component: <SceneAlignment localPosition={[0, 0, 0]} /> },
+      { name: 'Line', component: <SceneLine localPosition={[0, 0, 0]} /> },
       // { name: 'Calendar (local)', component: <CalendarView /> },
       // { name: 'Calendar (remote)', component: <SceneRemote /> },
     ]
-    this.state = { sceneIndex: 9 };
+    this.state = { sceneIndex: 1 };
     this.onNextScene = this.onNextScene.bind(this);
     this.onPreviousScene = this.onPreviousScene.bind(this);
   }
@@ -61,7 +67,7 @@ class BrowserApp extends React.Component {
         <view alignment={'center-center'} localPosition={[0, 1.3, 0]}>
           <button localPosition={[-0.5, 0, 0]} width={0.25} height={0.10} roundness={1} textSize={0.05} onClick={this.onPreviousScene}>Prev</button>
           <text localPosition={[0, 0.05, 0]} alignment={'top-center'} textAlignment={'center'} textSize={0.1} boundsSize={{ boundsSize: [0.7, 0.3], wrap: true }}>{scene.name}</text>
-          <button localPosition={[ 0.5, 0, 0]} width={0.25} height={0.10} roundness={1} textSize={0.05} onClick={this.onNextScene}>Next</button>
+          <button localPosition={[0.5, 0, 0]} width={0.25} height={0.10} roundness={1} textSize={0.05} onClick={this.onNextScene}>Next</button>
         </view>
         <view alignment={'center-center'}>
           {scene.component}
---
+++ src_mobile/test_scenes/sceneLinearLayout.js
@@ -0,0 +1,72 @@
+
+import React from 'react';
+// import { Button, GridLayout, Text } from 'react-native-magic-script';
+
+class SceneLinearLayout extends React.Component {
+
+  renderButton({
+    title,
+    enabled = true,
+    roundness = 0.0,
+    textColor = [0, 1, 0, 1],
+    textSize = 0.1,
+    width = 0.0,
+    height = 0.0,
+    onClick = () => { },
+  }) {
+    return (
+      <button
+        enabled={enabled}
+        textColor={textColor}
+        textSize={textSize}
+        roundness={roundness}
+        width={width}
+        height={height}
+        onClick={onClick}
+      >{title}</button>
+    );
+  }
+
+  render() {
+    return (
+      <view localPosition={this.props.localPosition}>
+        <linearLayout
+          localPosition={[0, -0.2, 0]}
+          alignment={'center-center'}
+          columns={3}
+          defaultItemAlignment={'bottom-center'}
+          orientation={'horizontal'}>
+
+          <linearLayout
+            defaultItemAlignment={'center-left'}
+            defaultItemPadding={[0.07, 0, 0.07, 0]}>
+            {this.renderButton({ textSize: 0.1, title: 'jeden' })}
+            {this.renderButton({ textSize: 0.11, title: 'dwa' })}
+            {this.renderButton({ textSize: 0.12, title: 'trzy' })}
+          </linearLayout>
+
+          <linearLayout
+            defaultItemAlignment={'center-right'}
+            defaultItemPadding={[0, 0.07, 0, 0.07]}>
+            {this.renderButton({ textSize: 0.1, title: 'one' })}
+            {this.renderButton({ textSize: 0.11, title: 'two' })}
+            {this.renderButton({ textSize: 0.12, title: 'three' })}
+          </linearLayout>
+
+          <linearLayout
+            defaultItemAlignment={'center-center'}>
+            {this.renderButton({ textSize: 0.10, title: 'alpha' })}
+            {this.renderButton({ textSize: 0.11, title: 'beta' })}
+            {this.renderButton({ textSize: 0.12, title: 'gamma' })}
+            {this.renderButton({ textSize: 0.13, title: 'delta' })}
+            {this.renderButton({ textSize: 0.14, title: 'epsilon' })}
+            {this.renderButton({ textSize: 0.15, title: 'zeta' })}
+          </linearLayout>
+
+        </linearLayout>
+      </view>
+    );
+  }
+}
+
+export { SceneLinearLayout };
