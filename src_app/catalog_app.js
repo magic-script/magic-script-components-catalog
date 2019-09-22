745ef03bba613c559aadc3eaa291843bb3852853
unknown <mateusz.wojcik@sofomo.com>
2019-09-22 15:07:54 +0200

    added scene black background

--- src_app/catalog_app.js
+++ src_app/catalog_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Text, Toggle, View } from 'magic-script-components';
+import { Button, Image, Text, Toggle, View } from 'magic-script-components';
 import { Grid } from './utils/grid';
 import {
   CalendarView,
@@ -101,6 +101,7 @@ class CatalogApp extends React.Component {
           <Text localPosition={[0, 0.05, 0]} alignment={'top-center'} textAlignment={'center'} textSize={0.1} boundsSize={{ boundsSize: [0.7, 0.3], wrap: true }}>{scene.name}</Text>
           <Button localPosition={[0.5, 0, 0]} width={0.25} height={0.10} roundness={1} textSize={0.05} onClick={this.onNextScene}>Next</Button>
           <Toggle localPosition={[0.13, 0.1, 0]} height={0.08} textSize={0.08} on={this.state.debug} onToggleChanged={this.onDebug} alignment={'bottom-center'}>grid</Toggle>
+          <Image localPosition={[0, -1, -0.1]} width={3} height={8} color={[0, 0, 0, 1]} ></Image>
         </View>
         <View alignment={'center-center'} localPosition={[viewPos.x, viewPos.y, viewPos.z]}>
           {this.renderGrid()}
