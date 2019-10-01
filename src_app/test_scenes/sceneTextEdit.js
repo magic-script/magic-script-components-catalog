89cd87b07ec9cc470b0207476bb38e9dadb7c5a5
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-01 13:31:46 +0200

    #21 Add missing fontParams.

--- src_app/test_scenes/sceneTextEdit.js
+++ src_app/test_scenes/sceneTextEdit.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 class SceneTextEdit extends React.Component {
 
-  renderTextEdit({ y, charSpacing, charLimit=0, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textPadding=[0.0, 0.03, 0.0, 0.03], textSize, multiline=false, password=false }) {
+  renderTextEdit({ y, charSpacing, charLimit=0, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textColor, textPadding=[0.0, 0.03, 0.0, 0.03], textSize, multiline=false, password=false, scrolling, weight, style }) {
     const size = { width: 0.9, height: multiline ? 0.25 : 0.07 };
+    const fontParams = {}
+    if (weight !== undefined) {
+      fontParams.weight = weight;
+    }
+    if (style !== undefined) {
+      fontParams.style = style;
+    }
     return (
       <view localPosition={[0, y, 0]}>
         <text 
@@ -16,6 +23,7 @@ class SceneTextEdit extends React.Component {
           alignment={'top-center'} 
           charSpacing={charSpacing}
           charLimit={charLimit}
+          fontParams={fontParams}
           hint={hint} 
           hintColor={hintColor}
           lineSpacing={lineSpacing} 
@@ -25,10 +33,10 @@ class SceneTextEdit extends React.Component {
           height={size.height} 
           password={password}
           textAlignment={textAlignment}
+          textColor={textColor}
           textPadding={textPadding}
           textSize={textSize} 
         >{text}</textEdit>
-        <line points={[[-0.5 * size.width, - size.height, 0], [0.5 * size.width, - size.height, 0]]} color={[1,1,,0.75]}/>
       </view>
     );
   }
@@ -36,11 +44,11 @@ class SceneTextEdit extends React.Component {
   render () {
     return (
       <view localPosition={this.props.localPosition}>
-        {this.renderTextEdit({ y: 0.45, title: 'Username', hint: 'Username', textAlignment: 'center', textColor: [0,1,0,1], textSize: 0.05 })}
+        {this.renderTextEdit({ y: 0.45, title: 'Username', hint: 'Username', textAlignment: 'center', textColor: [0,1,0,1], textSize: 0.05, weight: 'bold', style: 'normal' })}
         {this.renderTextEdit({ y: 0.25, title: 'Password', hint: 'Password', hintColor: [1,0,0,1], textAlignment: 'center', textSize: 0.05, password: true })}
         {this.renderTextEdit({ y: 0.05, title: 'E-mail', textEntry: 'email', textSize: 0.05 })}
         {this.renderTextEdit({ y:-0.15, title: 'Phone number', textEntry: 'numeric', text: '0048', charLimit: 15, charSpacing: 0.1 })}
-        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true })}
+        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true, weight: 'light', style: 'italic' })}
       </view>
     );
   }
