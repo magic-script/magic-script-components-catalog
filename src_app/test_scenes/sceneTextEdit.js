2f2ed76e9bef24050424f046d62f8c10f33e60c3
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-01 13:20:15 +0200

    #21 Update text edit scene.

--- src_app/test_scenes/sceneTextEdit.js
+++ src_app/test_scenes/sceneTextEdit.js
@@ -1,37 +1,46 @@
 import React from 'react';
 
 class SceneTextEdit extends React.Component {
+
+  renderTextEdit({ y, charSpacing, charLimit=0, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textPadding=[0.0, 0.03, 0.0, 0.03], textSize, multiline=false, password=false }) {
+    const size = { width: 0.9, height: multiline ? 0.25 : 0.07 };
+    return (
+      <view localPosition={[0, y, 0]}>
+        <text 
+          alignment={'bottom-left'} 
+          localPosition={[-0.5 * size.width, 0, 0]}
+          textColor={[1,1,1,1]}
+          textSize={0.05}
+        >{title}</text>
+        <textEdit 
+          alignment={'top-center'} 
+          charSpacing={charSpacing}
+          charLimit={charLimit}
+          hint={hint} 
+          hintColor={hintColor}
+          lineSpacing={lineSpacing} 
+          localPosition={[0, 0, 0]} 
+          multiline={multiline}
+          width={size.width} 
+          height={size.height} 
+          password={password}
+          textAlignment={textAlignment}
+          textPadding={textPadding}
+          textSize={textSize} 
+        >{text}</textEdit>
+        <line points={[[-0.5 * size.width, - size.height, 0], [0.5 * size.width, - size.height, 0]]} color={[1,1,,0.75]}/>
+      </view>
+    );
+  }
+
   render () {
     return (
       <view localPosition={this.props.localPosition}>
-        <text localPosition={[-0.3, 0.45, 0]} textSize={0.05}>Username</text>
-        <textEdit localPosition={[-0.3, 0.4, 0]} 
-            width={0.7} 
-            height={0.07} 
-            textSize={0.05}
-            hint={'Username'} 
-            textAlignment={'center'}
-            fontParams={{weight: "bold", style: "normal" }}
-            textColor={[0, 1, 0, 1]}/>
-
-        <text localPosition={[-0.3, 0.3, 0]} textSize={0.05}>Password</text>
-        <textEdit localPosition={[-0.3, 0.25, 0]} width={0.7} height={0.07} textAlignment={'center'} textSize={0.05} hint={'Password'} hintColor={[1, 0, 0, 1]} password></textEdit>
-
-        <text localPosition={[-0.3, 0.1, 0]} textSize={0.05}>E-mail</text>
-        <textEdit localPosition={[-0.3, 0.05, 0]} width={0.7} height={0.05} textEntry={"email"} hint={''}></textEdit>
-
-        <text localPosition={[-0.3, -0.05, 0]} textSize={0.05}>Phone number</text>
-        <textEdit localPosition={[-0.3, -0.1, 0]} width={0.7} height={0.05} textEntry={"numeric"} text={'0048'} charLimit={15} charSpacing={0.1}></textEdit>
-
-        <text localPosition={[-0.3, -0.2, 0]} textSize={0.05}>Additional info</text>
-        <textEdit localPosition={[-0.3, -0.25, 0]} 
-            scrolling={true} 
-            textPadding={[0.03, 0.03, 0.03, 0.03]}
-            fontParams={{weight: "light", style: "italic" }} 
-            width={0.7} 
-            height={0.25} 
-            hint={'*optional'}
-            multiline/>
+        {this.renderTextEdit({ y: 0.45, title: 'Username', hint: 'Username', textAlignment: 'center', textColor: [0,1,0,1], textSize: 0.05 })}
+        {this.renderTextEdit({ y: 0.25, title: 'Password', hint: 'Password', hintColor: [1,0,0,1], textAlignment: 'center', textSize: 0.05, password: true })}
+        {this.renderTextEdit({ y: 0.05, title: 'E-mail', textEntry: 'email', textSize: 0.05 })}
+        {this.renderTextEdit({ y:-0.15, title: 'Phone number', textEntry: 'numeric', text: '0048', charLimit: 15, charSpacing: 0.1 })}
+        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true })}
       </view>
     );
   }
