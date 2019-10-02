9f0d5462b74dd322cf7a6d27e7e7c43f632564cd
Dariusz Konieczko <dkonieczko@netizens.pl>
2019-10-02 08:26:36 +0200

    added missing properties, bigger charLimit

--- src_app/test_scenes/sceneTextEdit.js
+++ src_app/test_scenes/sceneTextEdit.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class SceneTextEdit extends React.Component {
 
-  renderTextEdit({ y, charSpacing, charLimit=0, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textColor, textPadding=[0.0, 0.03, 0.0, 0.03], textSize, multiline=false, password=false, scrolling, weight, style }) {
+  renderTextEdit({ y, charSpacing, charLimit=30, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textColor, textPadding=[0.0, 0.03, 0.0, 0.03], textSize, textEntry='normal', multiline=false, password=false, scrolling=false, weight, style }) {
     const size = { width: 0.9, height: multiline ? 0.25 : 0.07 };
     const fontParams = {}
     if (weight !== undefined) {
@@ -36,6 +36,8 @@ class SceneTextEdit extends React.Component {
           textColor={textColor}
           textPadding={textPadding}
           textSize={textSize} 
+          textEntry={textEntry}
+          scrolling={scrolling}
         >{text}</textEdit>
       </view>
     );
@@ -48,7 +50,7 @@ class SceneTextEdit extends React.Component {
         {this.renderTextEdit({ y: 0.25, title: 'Password', hint: 'Password', hintColor: [1,0,0,1], textAlignment: 'center', textSize: 0.05, password: true })}
         {this.renderTextEdit({ y: 0.05, title: 'E-mail', textEntry: 'email', textSize: 0.05 })}
         {this.renderTextEdit({ y:-0.15, title: 'Phone number', textEntry: 'numeric', text: '0048', charLimit: 15, charSpacing: 0.1 })}
-        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true, weight: 'light', style: 'italic' })}
+        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], charLimit: 80, textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true, weight: 'light', style: 'italic' })}
       </view>
     );
   }
