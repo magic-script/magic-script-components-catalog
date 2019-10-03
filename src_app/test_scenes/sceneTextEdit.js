2e2973af6b9a2295858f8358e97144aa10b1970d
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-03 11:18:40 +0200

    #21 Add Fill data button + changes to the way how data is passed to textField component.

--- src_app/test_scenes/sceneTextEdit.js
+++ src_app/test_scenes/sceneTextEdit.js
@@ -2,43 +2,61 @@ import React from 'react';
 
 class SceneTextEdit extends React.Component {
 
-  renderTextEdit({ y, charSpacing, charLimit=30, hint, hintColor=[0.75,0.75,0.75,0.75], lineSpacing, title='', text='', textAlignment='left', textColor, textPadding=[0.0, 0.03, 0.0, 0.03], textSize, textEntry='normal', multiline=false, password=false, scrolling=false, weight, style }) {
-    const size = { width: 0.9, height: multiline ? 0.25 : 0.07 };
-    const fontParams = {}
-    if (weight !== undefined) {
-      fontParams.weight = weight;
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: undefined,
+      password: undefined,
+      email: undefined,
+      phone: '0048',
+      info: undefined
     }
-    if (style !== undefined) {
-      fontParams.style = style;
+  }
+
+  onFillDataButtonAction = () => {
+    this.setState({
+      username: 'john-doe',
+      password: 'r_%77vkP*4Ts',
+      email: 'john.doe@mail.com',
+      phone: '123 456 789',
+      info: '• profession: gardener\n• experience: 8yrs\n• hobby: sport and book\n• languages: English, French'
+    });
+  }
+
+  renderTextEdit(y, title, textEditProps) {
+    const validProps = {};
+    for (let [key, value] of Object.entries(textEditProps)) {
+      if (value !== undefined) {
+        validProps[key] = value
+      }
+    }
+
+    const width = 0.9;
+    validProps.width = width;
+    validProps.height = validProps.multiline ? 0.25 : 0.07
+
+    const fontParams = {}
+    if (validProps.weight !== undefined) {
+      fontParams.weight = validProps.weight;
+      delete validProps.weight;
+    }
+    if (validProps.style !== undefined) {
+      fontParams.style = validProps.style;
+      delete validProps.style;
+    }
+    if (validProps.allCaps !== undefined) {
+      fontParams.allCaps = validProps.allCaps;
+      delete validProps.allCaps;
     }
     return (
       <view localPosition={[0, y, 0]}>
         <text 
           alignment={'bottom-left'} 
-          localPosition={[-0.5 * size.width, 0, 0]}
+          localPosition={[-0.5 * width, 0.02, 0]}
           textColor={[1,1,1,1]}
           textSize={0.05}
         >{title}</text>
-        <textEdit 
-          alignment={'top-center'} 
-          charSpacing={charSpacing}
-          charLimit={charLimit}
-          fontParams={fontParams}
-          hint={hint} 
-          hintColor={hintColor}
-          lineSpacing={lineSpacing} 
-          localPosition={[0, 0, 0]} 
-          multiline={multiline}
-          width={size.width} 
-          height={size.height} 
-          password={password}
-          textAlignment={textAlignment}
-          textColor={textColor}
-          textPadding={textPadding}
-          textSize={textSize} 
-          textEntry={textEntry}
-          scrolling={scrolling}
-        >{text}</textEdit>
+        <textEdit {...validProps} alignment={'top-center'} fontParameters={fontParams} />
       </view>
     );
   }
@@ -46,11 +64,12 @@ class SceneTextEdit extends React.Component {
   render () {
     return (
       <view localPosition={this.props.localPosition}>
-        {this.renderTextEdit({ y: 0.45, title: 'Username', hint: 'Username', textAlignment: 'center', textColor: [0,1,0,1], textSize: 0.05, weight: 'bold', style: 'normal' })}
-        {this.renderTextEdit({ y: 0.25, title: 'Password', hint: 'Password', hintColor: [1,0,0,1], textAlignment: 'center', textSize: 0.05, password: true })}
-        {this.renderTextEdit({ y: 0.05, title: 'E-mail', textEntry: 'email', textSize: 0.05 })}
-        {this.renderTextEdit({ y:-0.15, title: 'Phone number', textEntry: 'numeric', text: '0048', charLimit: 15, charSpacing: 0.1 })}
-        {this.renderTextEdit({ y:-0.35, title: 'Additional info', hint: '*optional', textPadding: [0.03, 0.03, 0.03, 0.03], charLimit: 80, textSize: 0.05, lineSpacing: 2, multiline: true, scrolling: true, weight: 'light', style: 'italic' })}
+        {this.renderTextEdit(0.45, 'Username', { hint: 'Username', text: this.state.username, textAlignment: 'center', textColor: [0,1,0,1], textSize: 0.05, weight: 'bold', style: 'normal' })}
+        {this.renderTextEdit(0.25, 'Password', { hint: 'Password', text: this.state.password, hintColor: [1,0,0,1], textAlignment: 'center', textSize: 0.05, password: true })}
+        {this.renderTextEdit(0.05, 'E-mail', { text: this.state.email, textEntry: 'email', textSize: 0.05 })}
+        {this.renderTextEdit(-0.15, 'Phone number', { text: this.state.phone, textEntry: 'numeric', textSize: 0.05, charLimit: 15, charSpacing: 0.1 })}
+        {this.renderTextEdit(-0.35, 'Additional info', { hint: '*optional', text: this.state.info, textPadding: [0.03, 0.03, 0.03, 0.03], textSize: 0.05, lineSpacing: 1.2, multiline: true, scrolling: true, weight: 'light', style: 'italic', allCaps: true })}
+        <button localPosition={[0.3, -0.8, 0]} textSize={0.05} onClick={this.onFillDataButtonAction}>Fill data</button>
       </view>
     );
   }
