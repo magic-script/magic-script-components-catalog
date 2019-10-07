c3932dad7336b3df761074d0179efc7ae8ebcff8
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-07 09:58:43 +0200

    System icons scene.

--- src_app/test_scenes/sceneSystemIcons.js
+++ src_app/test_scenes/sceneSystemIcons.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { SystemIcons } from '../utils/systemIcons';
 
 class SceneSystemIcons extends React.Component {
+  renderIcons() {
+    return SystemIcons.map((icon, index) => <image key={index} icon={icon} height={0.08} />);
+  }
+
   render () {
     return (
-      <view localPosition={this.props.localPosition}>
-        <image
-          filePath={require('../../assets/lumin_system_icons/Actions.png')}
-          width={0.3}
-          height={0.3}
-        />
+      <view localPosition={[0, 1.0, 0]}>
+        <gridLayout
+          debug={true}
+          alignment={'top-center'}
+          columns={14}
+          defaultItemPadding={[0.03, 0.008, 0.03, 0.008]}
+        >
+          {this.renderIcons()}
+        </gridLayout>
+        
       </view>
     );
   }
