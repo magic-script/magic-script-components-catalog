15e982b752d9a1211e5d85db14a250293293b265
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-15 13:58:09 +0200

    Enable slider scene.

--- src_app/catalog_app.js
+++ src_app/catalog_app.js
@@ -59,14 +59,14 @@ class CatalogApp extends React.Component {
       { name: 'Toggle', component: <SceneToggle localPosition={[0, 0, 0]} /> },
       { name: 'Spinner', component: <SceneSpinner localPosition={[0, 0, 0]} /> },
       { name: 'Progress bar', component: <SceneProgressBar localPosition={[0, 0, 0]} /> },
-      // { name: 'Slider', component: <SceneSlider localPosition={[0, 0, 0]} /> },
+      { name: 'Slider', component: <SceneSlider localPosition={[0, 0, 0]} /> },
       { name: 'Tic Tac Toe', component: <GameTicTacToe /> },
       { name: 'Clock', component: <SceneClock localPosition={[0, 0, 0]} /> },
       { name: 'Line', component: <SceneLine localPosition={[0, 0, 0]} /> },
       { name: 'Calendar (local)', component: <CalendarView localPosition={[0, 0, 0]} /> },
       { name: 'Calendar (remote)', component: <SceneRemote /> },
     ]
-    this.state = { sceneIndex: 2, showGrid: false };
+    this.state = { sceneIndex: 20, showGrid: false };
   }
 
   onNextScene = () => {
