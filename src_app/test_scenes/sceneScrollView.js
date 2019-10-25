53c5aa998e97f461d347c5d20f361d74857c3154
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-25 16:35:15 +0200

    #22 Add horizontal scroll.

--- src_app/test_scenes/sceneScrollView.js
+++ src_app/test_scenes/sceneScrollView.js
@@ -1,21 +1,44 @@
 import React from 'react';
 
+const Orientation = {
+  vertical: 'vertical',
+  horizontal: 'horizontal',
+};
+
 class SceneScrollView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isVertical: true };
+  }
+
+  onOrientationChanged = (e) => {
+    this.setState({ isVertical: e.On });
+  }
+
   render() {
-    const size = { width: 0.5, height: 0.5 };
+    const { isVertical } = this.state;
+    const contentSize = { width: 1.0, height: 1.25 };
+    const imageSize = { width: 1, height: 1};
     const aabb = { 
-      min: [-0.5 * size.width, -0.5 * size.height, -0.1], 
-      max: [0.5 * size.width, 0.5 * size.height, 0.1], 
+      min: [-0.5 * contentSize.width, -0.5 * contentSize.height, -0.1], 
+      max: [0.5 * contentSize.width, 0.5 * contentSize.height, 0.1], 
     };
+    const orientation = isVertical ? Orientation.vertical : Orientation.horizontal;
     return (
       <view localPosition={this.props.localPosition}>
-        <scrollView scrollBounds={aabb} scrollBarVisibility={'always'} scrollDirection={'vertical'}>
-          <scrollBar orientation={'vertical'} width={0.4} />
-          <gridLayout defaultItemAlignment={'center-center'} columns={1}>
-            <image width={size.width} height={size.height} color={[1,1,0.5,1]}/>
-            <image width={size.width} height={size.height} color={[1,0.5,1,1]}/>
-            <image width={size.width} height={size.height} color={[0.5,1,1,1]}/>
-            <image width={size.width} height={size.height} color={[1,1,1,1]}/>
+        <toggle localPosition={[0.2, 0.8, 0]} height={0.08} textSize={0.08} on={isVertical} onToggleChanged={this.onOrientationChanged}>{'Vertical scroll'}</toggle>
+        <scrollView scrollBounds={aabb} scrollBarVisibility={'always'} scrollDirection={orientation}>
+          <scrollBar orientation={orientation} width={contentSize.height} height={0.04} />
+          <gridLayout defaultItemAlignment={'center-center'} columns={2}>
+            <image width={imageSize.width} height={imageSize.height} color={[1,1,0.5,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,1,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,1,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,0.5,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,1,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,0.5,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,0.5,1]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,1,1,1]}/>
           </gridLayout>
         </scrollView>
       </view>
