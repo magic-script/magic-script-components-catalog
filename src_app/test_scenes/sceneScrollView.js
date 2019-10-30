44071ea65c69f118e644389c29385d6074049713
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-30 21:26:18 +0100

    Changes in scrollView scene.

--- src_app/test_scenes/sceneScrollView.js
+++ src_app/test_scenes/sceneScrollView.js
@@ -18,28 +18,29 @@ class SceneScrollView extends React.Component {
 
   render() {
     const { isVertical } = this.state;
-    const contentSize = { width: 1.0, height: 1.25 };
+    const contentSize = { width: 1.25, height: 1.25 };
     const imageSize = { width: 1, height: 1};
     const aabb = { 
       min: [-0.5 * contentSize.width, -0.5 * contentSize.height, -0.1], 
       max: [0.5 * contentSize.width, 0.5 * contentSize.height, 0.1], 
     };
     const orientation = isVertical ? Orientation.vertical : Orientation.horizontal;
+    const alpha = 0.2;
     return (
       <view localPosition={this.props.localPosition}>
         <toggle localPosition={[0.2, 0.8, 0]} height={0.08} textSize={0.08} on={isVertical} onToggleChanged={this.onOrientationChanged}>{'Vertical scroll'}</toggle>
-        <scrollView scrollBounds={aabb} scrollBarVisibility={'always'} scrollDirection={orientation}>
+        <scrollView debug scrollBounds={aabb} scrollBarVisibility={'always'} scrollDirection={orientation}>
           <scrollBar orientation={orientation} width={contentSize.height} height={0.04} />
-          <gridLayout defaultItemAlignment={'center-center'} columns={2}>
-            <image width={imageSize.width} height={imageSize.height} color={[1,1,0.5,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,1,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,1,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,0.5,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,1,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,0.5,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,0.5,1]}/>
-            <image width={imageSize.width} height={imageSize.height} color={[1,1,1,1]}/>
-          </gridLayout>
+          <linearLayout debug defaultItemAlignment={'center-center'} orientation={orientation}>
+            <image width={imageSize.width} height={imageSize.height} color={[1,1,0.5,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,1,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,1,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,0.5,0.5,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,1,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,1,0.5,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[0.5,0.5,0.5,alpha]}/>
+            <image width={imageSize.width} height={imageSize.height} color={[1,1,1,alpha]}/>
+          </linearLayout>
         </scrollView>
       </view>
     );
