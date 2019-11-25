2857654576b8a067bbeb009aef117a62a5f409a2
Nikolay Grozdanov <ngrozdanov@magicleap.com>
2019-11-25 15:25:40 -0500

    Fix Image component declaration

--- src_app/test_scenes/sceneRectLayout.js
+++ src_app/test_scenes/sceneRectLayout.js
@@ -35,7 +35,7 @@ class SceneRectLayout extends React.Component {
             Alignment.centerLeft, Alignment.centerCenter, Alignment.centerRight,
             Alignment.bottomLeft, Alignment.bottomCenter, Alignment.bottomRight
           ];
-      
+
           const currentIndex = alignments.indexOf(this.state.contentAlignment);
           const nextIndex = (currentIndex + 1) % alignments.length;
           this.setState({ contentAlignment: alignments[nextIndex].toString() });
@@ -47,23 +47,22 @@ class SceneRectLayout extends React.Component {
             Alignment.centerLeft, Alignment.centerCenter, Alignment.centerRight,
             Alignment.bottomLeft, Alignment.bottomCenter, Alignment.bottomRight
           ];
-      
+
           const currentIndex = alignments.indexOf(this.state.alignment);
           const nextIndex = (currentIndex + 1) % alignments.length;
           this.setState({ alignment: alignments[nextIndex].toString() });
         }
 
       selectNextSize = () => {
-          const widths = [
-            0.5, 1.5, 0.6
-          ]
-          const heights = [
-            0.5, 0.6, 1.5
-          ]
-          const currentWidthIndex = widths.indexOf(this.state.width)
+          const widths  = [0.5, 1.5, 0.6];
+          const heights = [0.5, 0.6, 1.5];
+
+          const currentWidthIndex  = widths.indexOf(this.state.width)
           const currentHeightIndex = heights.indexOf(this.state.height)
-          const nextWidthIndex = (currentWidthIndex + 1) % widths.length
+
+          const nextWidthIndex  = (currentWidthIndex + 1) % widths.length
           const nextHeightIndex = (currentHeightIndex + 1) % heights.length
+
           this.setState({ width: widths[nextWidthIndex], height: heights[nextHeightIndex]})
         }
 
@@ -76,7 +75,7 @@ class SceneRectLayout extends React.Component {
           const rectHeight = 1.0
           const textRect = `Rect: alignment=${alignment} contentAlignment=${contentAlignment} width=${rectWidth}, height=${rectHeight}`
           const textChild = `Content: width=${width} height=${height}`
-          
+
           return (
 
             <view localPosition={this.props.localPosition}>
@@ -92,24 +91,24 @@ class SceneRectLayout extends React.Component {
                   >
                     {this.renderButton({ width: width, height: height })}
                 </rectLayout>
-                
-                <image localPosition={rectPosition} alignment={alignment} width={1} height={1} useFrame/>
+
+                <image localPosition={rectPosition} alignment={alignment} width={1} height={1} useFrame={true} color={[0.25, 1, 0.50, 0.8]}/>
 
                 <linearLayout alignment={'top-center'} localPosition={[0, -1.5, 0.0]}>
-                  <button 
+                  <button
                       textSize={0.08}
                       roundness={0.0}
                       onClick={this.selectNextContentAlignment}>Change contentAlignment</button>
-                  <button 
+                  <button
                       textSize={0.08}
                       roundness={0.0}
                       onClick={this.selectNextAlignment}>Change rect alignment</button>
-                  <button 
+                  <button
                       textSize={0.08}
                       roundness={0.0}
                       onClick={this.selectNextSize}>Change content size</button>
                 </linearLayout>
-                
+
             </view>
           )
      }
