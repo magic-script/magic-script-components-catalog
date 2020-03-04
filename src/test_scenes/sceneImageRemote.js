d1cc1caa6256fdd9dd6cee0ba2091ed2bdcc27be
Paweł Leszkiewicz <pleszkiewicz@users.noreply.github.com>
2020-03-04 11:44:26 +0100

    Improvement/scene image remote (#119)
    * code clean up
    
    * Upgrade magic-script-polyfills version.

--- src/test_scenes/sceneImageRemote.js
+++ src/test_scenes/sceneImageRemote.js
@@ -1,28 +1,6 @@
 import React from 'react';
-// const RNFS = require('react-native-fs');
 
 class SceneImageRemote extends React.Component {
-
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = { localPath: '', downloadJobId: undefined };
-	// };
-
-	// componentDidMount() {
-	// 	const localPath = RNFS.DocumentDirectoryPath + '/fruits.png';
-	// 	const options = {
-	// 		fromUrl: 'https://homepages.cae.wisc.edu/~ece533/images/fruits.png',
-	// 		toFile: localPath
-	// 	};
-	// 	const value = RNFS.downloadFile(options);
-	// 	this.setState({ downloadJobId: value.jobId });
-	// 	value.promise.then((result) => this.setState({ localPath }))
-	// 	.catch((error) => console.log('[RNFS] Cannot download file. Reason: ', error));
-	// }
-
-	// componentWillUnmount() {
-	// 	RNFS.stopDownload(this.state.downloadJobId);
-	// }
 
 	render () {
 		const frameSize = 0.75;
@@ -49,21 +27,14 @@ class SceneImageRemote extends React.Component {
 					width={frameSize}
 					height={frameSize}
 					useFrame
-				></image>
+				/>
 				<image
 					filePath={'https://homepages.cae.wisc.edu/~ece533/images/cat.png'}
 					localPosition={[-0.425,-0.85,0]}
 					width={frameSize}
 					height={frameSize}
 					useFrame
-				></image>
-				{/* <image
-					filePath={this.state.localPath}
-					localPosition={[0.3,-0.85,0]}
-					width={frameSize}
-					height={frameSize}
-					useFrame
-				></image> */}
+				/>
 			</view>
 		);
 	}
