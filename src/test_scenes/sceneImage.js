f9c1b489c5ce1e18c8688f03b76d15ce6587eae5
Paweł Leszkiewicz <pleszkiewicz@users.noreply.github.com>
2020-04-02 15:44:30 +0200

    Update sceneImage.js component. (#133)

--- src/test_scenes/sceneImage.js
+++ src/test_scenes/sceneImage.js
@@ -1,40 +1,41 @@
 import React from 'react';
+import { Image, LinearLayout, Text  } from 'magic-script-components';
 
 class SceneImage extends React.Component {
+  renderImage(color, fit, useFrame) {
+    return (
+      <LinearLayout 
+        defaultItemAlignment={'top-center'}
+        defaultItemPadding={[0, 0, 0.01, 0]}
+      >
+        <Image
+          color={color}
+          filePath={require('../../assets/resources/board.png')}
+          fit={fit}
+          width={0.75}
+          height={0.5}
+          useFrame={useFrame}
+        />
+        <Text 
+          text={fit}
+          textAlignment={'center'}
+          textSize={0.06}
+        />
+      </LinearLayout>
+    );
+  }
+
   render () {
     return (
-      <view localPosition={this.props.localPosition}>
-        <image
-          filePath={require('../../assets/resources/DemoPicture1.jpg')}
-          localPosition={[-0.25, 0.25, 0]}
-          width={0.25}
-          height={0.25}
-        />
-        <image
-          filePath={require('../../assets/resources/DemoPicture2.jpg')}
-          localPosition={[-0.25, 0, 0]}
-          width={0.25}
-          height={0.25}
-        />
-        <image
-          filePath={require('../../assets/resources/DemoPicture3.jpg')}
-          localPosition={[0.125, 0.125, 0]}
-          width={0.5}
-          height={0.5}
-        ></image>
-        <image
-          filePath={require('../../assets/resources/DemoPicture4.jpg')}
-          localPosition={[-0.125, -0.25, 0]}
-          width={0.5}
-          height={0.25}
-        ></image>
-        <image
-          filePath={require('../../assets/resources/DemoPicture5.jpg')}
-          localPosition={[0.25, -0.25, 0]}
-          width={0.25}
-          height={0.25}
-        ></image>
-      </view>
+      <LinearLayout 
+        alignment={'center-center'}
+        defaultItemAlignment={'center-center'}
+        defaultItemPadding={[0.05, 0, 0.05, 0]}
+      >
+        {this.renderImage('yellow', 'stretch', false)}
+        {this.renderImage('orange', 'aspect-fill', true)}
+        {this.renderImage('white', 'aspect-fit', true)}
+      </LinearLayout>
     );
   }
 }
