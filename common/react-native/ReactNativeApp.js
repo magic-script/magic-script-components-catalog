139d76fd9f400cd5b9029a5d972a0444c4e1a847
Dariusz Konieczko <d.konieczko@nomtek.com>
2020-04-09 10:42:15 +0200

    additional camera component

--- common/react-native/ReactNativeApp.js
+++ common/react-native/ReactNativeApp.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, View, Switch, Text, Button } from 'react-native';
+import React, {PureComponent } from 'react';
+import { StyleSheet, View, Switch, Text, Button, TouchableOpacity } from 'react-native';
 import { ARView } from 'magic-script-components-react-native';
 import { setInitialDeeplink } from '../../global/globalVariables'
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { RNCamera } from 'react-native-camera';
 
 export default class ReactNativeApp extends React.Component {
 
@@ -56,13 +57,44 @@ class Page1 extends React.Component {
    }
 }
 
-class Page2 extends React.Component {
+class Page2 extends PureComponent {
   render() {
      return(
-       <Text>Page 2</Text> 
+      <View style={styles.cameraContainer}>
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        captureAudio={false}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
      );
   }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -70,6 +102,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
   },
 
   containerHorizontal: {
@@ -86,5 +124,22 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#555555',
+  },
+
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   }
+
 });
