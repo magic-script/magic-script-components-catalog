ca9adcc8e2a21edb801946fce9b73d4441125ff9
Dariusz Konieczko <d.konieczko@nomtek.com>
2020-04-09 09:58:06 +0200

    2 pages

--- common/react-native/ReactNativeApp.js
+++ common/react-native/ReactNativeApp.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { StyleSheet, View, Switch, Text } from 'react-native';
+import { StyleSheet, View, Switch, Text, Button } from 'react-native';
 import { ARView } from 'magic-script-components-react-native';
 import { setInitialDeeplink } from '../../global/globalVariables'
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 export default class ReactNativeApp extends React.Component {
+
+  render() {
+    const Stack = createStackNavigator();
+
+    return (
+
+      <NavigationContainer>
+         <Stack.Navigator>
+            <Stack.Screen name="Page1" component={Page1} options={{title: 'Welcome'}}/>
+            <Stack.Screen name="Page2" component={Page2} />
+         </Stack.Navigator>
+      </NavigationContainer>
+
+    );
+  }
+}
+
+class Page1 extends React.Component {
 
   constructor(props) {
     super(props);
@@ -17,17 +38,29 @@ export default class ReactNativeApp extends React.Component {
     this.setState({ planeDetection: !planeDetection })
   }
 
-  render() {
-    const { planeDetection } = this.state;
-    return (
-      <View style={styles.container}>
-        <ARView style={styles.arView} planeDetection={planeDetection} rendersContinuously={true} />
-        <View style={styles.containerHorizontal}>
-            <Text>Plane detection</Text>
-            <Switch value={planeDetection} onValueChange={this.onPlaneDetectionChanged}/>    
+   render() {
+
+      const { planeDetection } = this.state;
+      const navigation = this.props.navigation
+
+      return (
+        <View style={styles.container}>
+          <ARView style={styles.arView} planeDetection={planeDetection} rendersContinuously={true} />
+          <View style={styles.containerHorizontal}>
+              <Text>Plane detection</Text>
+              <Switch value={planeDetection} onValueChange={this.onPlaneDetectionChanged}/>    
+          </View>
+          <Button title="Go to Page2" onPress={() => navigation.navigate('Page2')}/>
         </View>
-      </View>
-    );
+      );
+   }
+}
+
+class Page2 extends React.Component {
+  render() {
+     return(
+       <Text>Page 2</Text> 
+     );
   }
 }
 
