401f45fde1c94fa3235f4a2e13764cd7ea3a9c0a
Grzegorz Dec <grzegorz.dec@blstream.com>
2020-04-22 14:10:04 +0200

    SceneDialog update (#136)

--- src/test_scenes/sceneDialog.js
+++ src/test_scenes/sceneDialog.js
@@ -1,14 +1,60 @@
 import React from "react";
+import { LinearLayout, Button, View, Text, Dialog, ToggleGroup, Toggle } from 'magic-script-components';
+
+const DialogType = [
+  "timed",
+  "no-action",
+  "single-action",
+  "dual-action"
+]
+
+const ButtonType = [
+  "icon",
+  "icon-with-label",
+  "text",
+  "text-with-icon"
+]
+
+const LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 class SceneDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showDialog: false,
-      showDialogWithIcons: false,
-      showDialogLongText: false,
-      showDialogTimeExpire: false
+      dialogTypeIndex: 3,
+      buttonTypeIndex: 3
     };
+  }
+
+  renderDialogTypeRadio(index, selectedIndex, onChanged) {
+    const dialogType = DialogType[index];
+    const on = (index === selectedIndex);
+    return (
+      <Toggle
+        height={0.075}
+        on={on}
+        text={dialogType}
+        textSize={0.075}
+        type={'radio'}
+        onToggleChanged={() => onChanged(index)}
+      />
+    );
+  }
+
+  renderButtonTypeRadio(index, selectedIndex, onChanged) {
+    const buttonType = ButtonType[index];
+    const on = (index === selectedIndex);
+    return (
+      <Toggle
+        height={0.075}
+        on={on}
+        text={buttonType}
+        textSize={0.075}
+        type={'radio'}
+        onToggleChanged={() => onChanged(index)}
+      />
+    );
   }
 
   renderButton({
@@ -19,10 +65,10 @@ class SceneDialog extends React.Component {
     textSize = 0.08,
     width = 0.0,
     height = 0.0,
-    onClick = () => {}
+    onClick = () => { }
   }) {
     return (
-      <button
+      <Button
         enabled={enabled}
         textColor={textColor}
         textSize={textSize}
@@ -32,7 +78,7 @@ class SceneDialog extends React.Component {
         onClick={onClick}
       >
         {title}
-      </button>
+      </Button>
     );
   }
 
@@ -52,92 +98,58 @@ class SceneDialog extends React.Component {
     this.setState({ showDialog: false });
   };
 
-  onDialogConfirmed_icons = event => {
-    console.log("onDialogConfirmed event received: ", event);
-    this.setState({ showDialogWithIcons: false });
-  };
-
-  onDialogCanceled_icons = event => {
-    console.log("onDialogCanceled event received: ", event);
-    this.setState({ showDialogWithIcons: false });
-  };
-
-  onDialogConfirmed_long = event => {
-    console.log("onDialogConfirmed event received: ", event);
-    this.setState({ showDialogLongText: false });
-  };
-
-  onDialogCanceled_long = event => {
-    console.log("onDialogCanceled event received: ", event);
-    this.setState({ showDialogLongText: false });
-  };
-
   onDialogTimeExpired = event => {
     console.log("onDialogTimeExpired event received: ", event);
     this.setState({ showDialogTimeExpire: false });
   };
 
+  onDialogTypeChanged = (index) => {
+    this.setState({ dialogTypeIndex: index });
+  }
+
+  onButtonTypeChanged = (index) => {
+    this.setState({ buttonTypeIndex: index });
+  }
+
   render() {
     const {
       showDialog,
-      showDialogWithIcons,
-      showDialogLongText,
-      showDialogTimeExpire
+      dialogTypeIndex,
+      buttonTypeIndex
     } = this.state;
-    const showButtons = !(showDialog || showDialogWithIcons || showDialogLongText || showDialogTimeExpire);
+    const showButtons = !showDialog;
+
+    const expireTime = dialogTypeIndex === 0 ? { expireTime: 3 } : null;
+    const confirmIcon = (dialogTypeIndex !== 0 && buttonTypeIndex !== 2) ? { confirmIcon: "thumbs-up" } : null;
+    const cancelIcon = (dialogTypeIndex !== 0 && buttonTypeIndex !== 2) ? { cancelIcon: "thumbs-down" } : null;
+    const confirmText = (dialogTypeIndex !== 0 && buttonTypeIndex !== 0) ? { confirmText: "Confirm" } : null;
+    const cancelText = (dialogTypeIndex !== 0 && buttonTypeIndex !== 0) ? { cancelText: "Cancel" } : null;
+    const buttonType = { buttonType: ButtonType[buttonTypeIndex] }
+
     return (
-      <view localPosition={this.props.localPosition}>
-        <view localScale={[2,2,2]} localPosition={[0,0.3,0.2]}>
+      <View localPosition={this.props.localPosition}>
+
+        <View localScale={[2, 2, 2]} localPosition={[0, 0.3, 0.2]}>
           {showDialog && (
-            <dialog
+            <Dialog
               onDialogConfirmed={this.onDialogConfirmed_normal}
               onDialogCanceled={this.onDialogCanceled_normal}
               title={"Dialog title text"}
-              message={"Dialog message text"}
-              confirmText={"Confirm"}
-              cancelText={"Cancel"}
+              message={LoremIpsum}
+              {...confirmText}
+              {...confirmIcon}
+              {...cancelText}
+              {...cancelIcon}
+              {...expireTime}
+              {...buttonType}
+              type={DialogType[dialogTypeIndex]}
             />
           )}
-          {showDialogWithIcons && (
-            <dialog
-              onDialogConfirmed={this.onDialogConfirmed_icons}
-              onDialogCanceled={this.onDialogCanceled_icons}
-              title={"Dialog title text"}
-              message={"Dialog message text"}
-              confirmText={"YES"}
-              confirmIcon={"thumbs-up"}
-              cancelText={"NO"}
-              cancelIcon={"thumbs-down"}
-            />
-          )}
-          {showDialogLongText && (
-            <dialog
-              onDialogConfirmed={this.onDialogConfirmed_long}
-              onDialogCanceled={this.onDialogCanceled_long}
-              title={"Dialog title text"}
-              message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus nisl a felis accumsan, ac sollicitudin lorem sagittis. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ut justo a vulputate. Maecenas sit amet purus porttitor, tempus ante a, commodo mi. Quisque non tempus ipsum, in ornare justo. Nam ut ex pretium, maximus tellus imperdiet, tempor purus. In viverra volutpat sem nec posuere. Morbi eu erat sem. Phasellus at sapien quis arcu consequat sodales. Donec iaculis, nisi et tempor iaculis, est enim vulputate metus, et vestibulum enim nisl sed urna. Etiam dictum nibh non commodo eleifend. Phasellus ut sollicitudin dolor. Nunc quis ultrices orci. Sed convallis ex vel dignissim varius. Sed pretium porta lacus, ac venenatis magna malesuada posuere. Aenean maximus suscipit dolor quis egestas. Suspendisse venenatis non lectus quis fringilla. Nullam viverra tristique fringilla. Integer dictum diam arcu, at congue ex sagittis et. Duis arcu erat, elementum ac dui sagittis, varius rutrum nunc. Nulla iaculis ipsum eu nisi sollicitudin, non suscipit enim interdum. Integer tincidunt nisl metus, eu imperdiet felis accumsan ac. Proin lacinia commodo dui, quis hendrerit augue dignissim id. Nullam rhoncus, purus quis faucibus scelerisque, nunc enim dictum eros, ut rutrum libero lacus eget magna. Donec cursus ultrices luctus. Sed sit amet augue felis. Nam volutpat lorem iaculis nulla feugiat cursus eget eu arcu. Donec congue velit id mi facilisis vehicula. Cras convallis orci vel dolor rutrum, feugiat posuere nulla faucibus. Vestibulum in viverra dui. Sed accumsan dignissim lectus a elementum. Vivamus a hendrerit nisi. In vehicula fringilla felis eu posuere. Nunc mattis pellentesque aliquam. Duis aliquam ullamcorper cursus. Sed laoreet justo lectus, eu mollis justo hendrerit at. Praesent facilisis vestibulum tortor, quis tempor libero dapibus nec. Duis eleifend varius dignissim. Vestibulum sem dolor, sodales eu consequat nec, blandit nec elit. Praesent commodo ligula metus, non lobortis augue dignissim ac. Nunc efficitur posuere tortor ut interdum. Vivamus sagittis neque ac eleifend consectetur. Sed tincidunt, neque sed maximus imperdiet, ligula eros condimentum ex, vitae suscipit nibh lacus id lorem. In pellentesque nisl ligula, nec bibendum diam commodo a. Sed at nunc dolor. Aenean id ultricies odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat quam eu finibus semper. Nunc sit amet venenatis tellus. Nullam elit turpis, mattis sed porta ut, tempor in est. Cras sed lorem sed metus elementum tincidunt porta luctus libero. Nulla varius tortor eros, sit amet porttitor nulla faucibus id. Donec sit amet dignissim ante. Aliquam volutpat eleifend nibh nec congue. Sed faucibus felis ac semper posuere. Phasellus luctus rhoncus lacus, eu maximus lacus viverra a. Vivamus eu metus dapibus, tempor dui sed, finibus nisi. Etiam quis mi tortor. Nulla facilisi."}
-              confirmText={"Confirm"}
-              confirmIcon={"check"}
-              cancelText={"Cancel"}
-              cancelIcon={"close"}
-              scrolling={true}
-            />
-          )}
-          {showDialogTimeExpire && (
-            <dialog
-              onDialogTimeExpired={this.onDialogTimeExpired}
-              title={"Auto dismiss"}
-              message={"This dialog will expire in 3 seconds."}
-              type={"timed"}
-              expireTime={3.0}
-            />
-          )}
-        </view>
+        </View>
 
         {showButtons && (
-          <linearLayout
+          <LinearLayout
             alignment={"center-center"}
-            columns={1}
             defaultItemAlignment={"center-center"}
             defaultItemPadding={[0.07, 0, 0.07, 0]}
             localPosition={[0, 0.3, 0]}
@@ -148,23 +160,27 @@ class SceneDialog extends React.Component {
               onClick: this.onButtonClick({ showDialog: true })
             })}
 
-            {this.renderButton({
-              title: "Show dialog (short text + icons)",
-              onClick: this.onButtonClick({ showDialogWithIcons: true })
-            })}
-
-            {this.renderButton({
-              title: "Show dialog (long text + icons)",
-              onClick: this.onButtonClick({ showDialogLongText: true })
-            })}
-
-            {this.renderButton({
-              title: "Show dialog (time expiration)",
-              onClick: this.onButtonClick({ showDialogTimeExpire: true })
-            })}
-          </linearLayout>
+            <LinearLayout orientation="horizontal">
+              <ToggleGroup>
+                <LinearLayout orientation={'vertical'}>
+                  {this.renderDialogTypeRadio(0, dialogTypeIndex, this.onDialogTypeChanged)}
+                  {this.renderDialogTypeRadio(1, dialogTypeIndex, this.onDialogTypeChanged)}
+                  {this.renderDialogTypeRadio(2, dialogTypeIndex, this.onDialogTypeChanged)}
+                  {this.renderDialogTypeRadio(3, dialogTypeIndex, this.onDialogTypeChanged)}
+                </LinearLayout>
+              </ToggleGroup>
+              <ToggleGroup>
+                <LinearLayout orientation={'vertical'}>
+                  {this.renderButtonTypeRadio(0, buttonTypeIndex, this.onButtonTypeChanged)}
+                  {this.renderButtonTypeRadio(1, buttonTypeIndex, this.onButtonTypeChanged)}
+                  {this.renderButtonTypeRadio(2, buttonTypeIndex, this.onButtonTypeChanged)}
+                  {this.renderButtonTypeRadio(3, buttonTypeIndex, this.onButtonTypeChanged)}
+                </LinearLayout>
+              </ToggleGroup>
+            </LinearLayout>
+          </LinearLayout>
         )}
-      </view>
+      </View>
     );
   }
 }
