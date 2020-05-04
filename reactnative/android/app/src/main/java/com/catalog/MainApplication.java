41914a133424498cee806884962f18c1d835c74c
darek607 <d.konieczko@nomtek.com>
2020-05-04 16:13:28 +0200

    filesystem support (#147)

--- reactnative/android/app/src/main/java/com/catalog/MainApplication.java
+++ reactnative/android/app/src/main/java/com/catalog/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.List;
+import com.rnfs.RNFSPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
