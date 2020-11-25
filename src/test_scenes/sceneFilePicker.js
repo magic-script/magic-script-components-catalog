1e336bf9b63501ac7b814e4c6c9ca31f19c2fcdc
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2020-11-25 14:14:10 +0100

    Exclude FilePicker from the scene.

--- src/test_scenes/sceneFilePicker.js
+++ src/test_scenes/sceneFilePicker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Alignment, Audio, AudioAction, FilePicker, Image, LinearLayout, Toggle, View } from 'magic-script-components';
+import { Alignment, Audio, AudioAction, /*FilePicker,*/ Image, LinearLayout, Toggle, View } from 'magic-script-components';
 
 class SceneFilePicker extends React.Component {
 
@@ -21,11 +21,12 @@ class SceneFilePicker extends React.Component {
           defaultItemAlignment={Alignment.centerLeft}
           defaultItemPadding={[0.07, 0, 0.07, 0]}
         >
-          <FilePicker width={1} height={0.4} fileType="image/*" onFileSelected={this.onImageSelected}>Image Picker</FilePicker>
+          {/* FilePicker is not supported on Lumin */}
+          {/* <FilePicker width={1} height={0.4} fileType="image/*" onFileSelected={this.onImageSelected}>Image Picker</FilePicker> */}
 
           <Image path={image} width={0.75} height={0.75} />
 
-          <FilePicker width={1} height={0.4} fileType="audio/*" onFileSelected={this.onAudioSelected}>Audio Picker</FilePicker>
+          {/* <FilePicker width={1} height={0.4} fileType="audio/*" onFileSelected={this.onAudioSelected}>Audio Picker</FilePicker> */}
 
           <Audio action={AudioAction.start} looping={true} mute={mute} path={audio} vaolume={0.5} />
           <Toggle on={!mute} fontSize={0.08} height={0.1} onToggleChanged={this.onMute}>Play sound</Toggle>
